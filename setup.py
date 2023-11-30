7f30120caf1a8c0b03bbde1eb4ab6631ad117db7
susannawinkelbauer <77398537+susannawinkelbauer@users.noreply.github.com>
2023-11-30 10:30:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup
 
+setup(
+    name = 'StraitFlux',
+    packages = ['StraitFlux'],
+    version = '1.0.1',
+    install_requires=['xarray', 'netcdf4', 'xesmf', 'xmip', 'tqdm', 'matplotlib', 'dask'], #external packages as dependencies
+    python_requires=">=3.10",
+    description = 'StraitFlux',
+    long_description = 'StraitFlux - calculates oceanic transports and crosssections on different model grids.',
+    author = 'Susanna Winkelbauer',
+    author_email = 'susanna.winkelbauer@univie.ac.at',
+    url = 'https://github.com/susannawinkelbauer/StraitFlux',
+    download_url = 'https://github.com/susannawinkelbauer/StraitFlux/archive/refs/tags/v1.0.1.tar.gz',
+    keywords = ['python', 'oceanic transports','curvilinear grids'],
+    classifiers = ["Intended Audience :: Science/Research", "Programming Language :: Python :: 3",],
+)
